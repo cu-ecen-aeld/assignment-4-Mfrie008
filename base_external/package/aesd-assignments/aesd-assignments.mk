75628040dfa465541ce7c684d843207acc68383d
Matthew Friedman <mafr6311@colorado.edu>
2026-07-29 11:00:55 -0700

    latest ver with assignment4 folder

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 832bf85cfd179df4e72c4a29837b0e7e3b8ca922
+AESD_ASSIGNMENTS_VERSION = 915e0b807851b3e4c1074a000080a9b5c4074a0b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
